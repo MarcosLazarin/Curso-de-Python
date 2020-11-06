76e78419d8be3114c0f5d19415c34cd909029e28
MarcosLazarin <73917140+MarcosLazarin@users.noreply.github.com>
2020-11-05 21:01:13 -0300

    Delete ex30.py

--- ex30.py
+++
@@ -1,7 +0,0 @@
-#Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
-n = int(input('Digite um número: '))
-r = n % 2
-if r == 0:
-    print('O número {} é par.'.format(n))
-else:
-    print('O número {} é ímpar.'.format(n))
